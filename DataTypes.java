5964eb65cd6ac92a45c25fe11a418bba32853c34
dannnnnno-o <dannnnnnnno.o@gmail.com>
2025-11-21 14:52:44 +0800

    primitive types

---
+++ DataTypes.java
@@ -0,0 +1,17 @@
+public class DataTypes {
+public static void main(String[] args){
+    int x = 2;
+    int y = 3;
+    int z = x + y;
+    System.out.println("z value: " + z);
+
+    boolean isAlive = true;
+
+    // System.out.println(isAlive);
+
+    if(isAlive){
+        System.out.println("Die.");
+    }
+}
+
+}
